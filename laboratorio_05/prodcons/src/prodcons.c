dfdc705719f5b47b0df37c533ebf88763ef8cac3
Victor Feitosa Lourenco <victor.240398@hotmail.com>
2021-07-23 19:16:08 -0300

    Corrections to Lab 05

--- laboratorio_05/prodcons/src/prodcons.c
+++ laboratorio_05/prodcons/src/prodcons.c
@@ -5,6 +5,77 @@
 
 #define BUFFER_SIZE 8
 
+osThreadId_t consumer_id;
+osSemaphoreId_t empty_id, full_id;
+uint8_t buffer[BUFFER_SIZE];
+
+// To be used each time the button handler is called
+uint8_t index_i = 0, count = 0;
+
+
+// Acts as the producer
+void GPIOJ_Handler(void){
+  ButtonIntClear(USW1);
+  
+  // Only proceed if there is space available, otherwise, do not block ISR
+  if(osSemaphoreAcquire(empty_id, 0) == osOK){
+      buffer[index_i] = count; // if so, add to buffer
+      osSemaphoreRelease(full_id); // signal one less space
+      
+      index_i++; // increment buffer input index
+      if(index_i >= BUFFER_SIZE)
+        index_i = 0;
+      
+      count++;
+      count &= 0x0F; // produce new information
+  }
+} // producer
+
+
+void consumer(void *arg){
+  uint8_t index_o = 0, state;
+  
+  while(1){
+    osSemaphoreAcquire(full_id, osWaitForever); // is there space available?
+    state = buffer[index_o]; // if so, remove from buffer
+    osSemaphoreRelease(empty_id); // signal one more space
+    
+    index_o++; // increment buffer output index
+    if(index_o >= BUFFER_SIZE)
+      index_o = 0;
+    
+    LEDWrite(LED4 | LED3 | LED2 | LED1, state); // present consumed information
+    osDelay(500);
+  } // while
+} // consumer
+
+
+void main(void){
+  SystemInit();
+  LEDInit(LED4 | LED3 | LED2 | LED1);
+  ButtonInit(USW1);
+  ButtonIntEnable(USW1);
+
+  osKernelInitialize();
+
+  consumer_id = osThreadNew(consumer, NULL, NULL);
+
+  empty_id = osSemaphoreNew(BUFFER_SIZE, BUFFER_SIZE, NULL); // available spaces = BUFFER_SIZE
+  full_id = osSemaphoreNew(BUFFER_SIZE, 0, NULL); // occupied spaces = 0
+  
+  if(osKernelGetState() == osKernelReady)
+    osKernelStart();
+
+  while(1);
+} // main
+
+
+/*
+
+// Old attempt, with two threads
+
+#define BUFFER_SIZE 8
+
 osThreadId_t producer_id, consumer_id;
 osSemaphoreId_t empty_id, full_id;
 uint8_t buffer[BUFFER_SIZE];
@@ -74,3 +145,5 @@ void main(void){
 
   while(1);
 } // main
+
+*/
