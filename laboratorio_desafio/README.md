2a69bcac8959f4160bdd8a2c878400a51aa3ecdd
Victor Feitosa Lourenco <victor.240398@hotmail.com>
2021-07-30 01:00:39 -0300

    Lab desafio

---
+++ laboratorio_desafio/README.md
@@ -0,0 +1,10 @@
+## Lab 06 - Challenge
+
+A culmination of several embedded systems concepts into one lab. This lab is loosely based on the project “blinky” in the workspace “TM4C1294_RTOS_IAR9-main” (from the previously mentioned [repository (RTOS)](https://github.com/ELF74-SisEmb/TM4C1294_RTOS_IAR9)), although it also used concepts from projects "prodcons" and "tarefas" as well:
+* osMessageQueueId_t initialization and functions;
+* Creation of appropriate diagrams to model the software;
+* Combine threads, mutexes, queues, flags and interruptions.
+
+### HEADS-UP
+
+This lab is not yet fully functional.
---
+++ laboratorio_desafio/lab_des/src/lab_des.c
@@ -0,0 +1,170 @@
+/* -------------------------------------------------------------------------- 
+ * Observations:
+ *      Attempting to use the following example as reference for PWM
+ *    ti\TivaWare_C_Series-2.1.4.178\examples\peripherals\pwm\reload_interrupt.c
+ *
+ *      NOT FINISHED!
+ *---------------------------------------------------------------------------*/
+
+#include "system_tm4c1294.h" // CMSIS-Core
+#include "driverleds.h" // device drivers
+#include "cmsis_os2.h" // CMSIS-RTOS
+#include "driverbuttons.h"
+
+#define QUEUE_SIZE 8
+
+// Struct to be passed as argument to the threads
+struct selection
+{
+   int led;             // select which LED to use
+   int act_period;      // select the activation period
+};
+
+osThreadId_t tid_led1;                /* Thread id of thread: phase_a      */
+osThreadId_t tid_led2;                /* Thread id of thread: phase_b      */
+osThreadId_t tid_led3;                /* Thread id of thread: phase_c      */
+osThreadId_t tid_led4;                /* Thread id of thread: phase_d      */
+osThreadId_t tid_controladora;                 /* Thread id of thread: clock        */
+
+osMutexId_t phases_mut_id;
+osMessageQueueId_t mid_MsgQueue;
+
+const osMutexAttr_t Phases_Mutex_attr = {
+  "PhasesMutex",                            // human readable mutex name
+  osMutexRecursive | osMutexPrioInherit,    // attr_bits
+  NULL,                                     // memory for control block   
+  0U                                        // size for control block
+};
+
+
+volatile uint8_t pwm_event = 0, led_event = 0;
+
+// Identify which button was pressed to flag the correct event
+void GPIOJ_Handler(void){
+  if(!GPIO_PORTJ_DATA_R&0x10){
+    ButtonIntClear(USW1);
+    pwm_event = 1;
+  }
+  else(!GPIO_PORTJ_DATA_R&0x01)){
+    ButtonIntClear(USW2);
+    led_event = 1;
+  }
+}
+
+
+/*----------------------------------------------------------------------------
+ *      Switch LED on (PROTECTED)
+ *---------------------------------------------------------------------------*/
+void Switch_On (int led, uint32_t pwm_state) {
+  osMutexAcquire(phases_mut_id, osWaitForever); // try to acquire mutex
+  // PROPERLY IMPLEMENT PWM
+  LEDOn(led);
+  osMutexRelease(phases_mut_id);
+}
+
+/*----------------------------------------------------------------------------
+ *      Switch LED off (PROTECTED)
+ *---------------------------------------------------------------------------*/
+void Switch_Off (int led) {
+  osMutexAcquire(phases_mut_id, osWaitForever); // try to acquire mutex
+  LEDOff(led);
+  osMutexRelease(phases_mut_id);
+}
+
+
+/*----------------------------------------------------------------------------
+ *      acionadora: Activates an argument LED
+ *---------------------------------------------------------------------------*/
+void acionadora (void *argument) {
+  struct selection* select = argument;  // allow argument use inside the thread
+  uint32_t pwm_state;
+  for (;;) {
+    // Will only activate if the controler function unblocks this specific task
+    osThreadFlagsWait(0x0001, osFlagsWaitAny ,osWaitForever);    /* wait for an event flag 0x0001 */
+    // Retrieve correspondent PWM state
+    if (osMessageQueueGet(mid_MsgQueue, &pwm_state, NULL, 0U) == osOK){
+      Switch_On(select->led, pwm_state);
+      osDelay(select->act_period);
+      Switch_Off(select->led);
+    }
+  }
+}
+
+/*----------------------------------------------------------------------------
+ *      acionadora: Controls user input and which LED task to call
+ *---------------------------------------------------------------------------*/
+void controladora (void *argument) {
+  osThreadId_t acionador_atual = tid_led1;
+  uint32_t pwm = 1;
+  
+  for (;;) {
+    if (pwm_event){
+      pwm_event = 0;
+      
+      pwm++;
+      pwm &= 0x0A; // produce new information
+    }
+    else  if(led_event){
+      led_event = 0;
+      if (acionador_atual==tid_led1){
+        acionador_atual = tid_led2;
+      }
+      else if (acionador_atual==tid_led2){
+        acionador_atual = tid_led3;
+      }
+      else if (acionador_atual==tid_led3){
+        acionador_atual = tid_led4;
+      }
+      else if (acionador_atual==tid_led4){
+        acionador_atual = tid_led1;
+      }
+    }
+    
+    if(osMessageQueuePut(mid_MsgQueue, &pwm, 0U, 0U) == osOK){
+      osThreadFlagsSet(acionador_atual, 0x0001);
+      osDelay(500);
+    }
+    
+  }
+}
+
+
+/*----------------------------------------------------------------------------
+ *      Main: Initialize
+ *---------------------------------------------------------------------------*/
+void app_main (void *argument) {
+  struct selection first = {LED1, 500};
+  struct selection second = {LED2, 500};
+  struct selection third = {LED3, 500};
+  struct selection fourth = {LED4, 500};
+  
+  tid_controladora  = osThreadNew(controladora,  NULL, NULL);
+  tid_led1 = osThreadNew(acionadora, &first, NULL);
+  tid_led2 = osThreadNew(acionadora, &second, NULL);
+  tid_led3 = osThreadNew(acionadora, &third, NULL);
+  tid_led4 = osThreadNew(acionadora, &fourth, NULL);
+
+  phases_mut_id = osMutexNew(&Phases_Mutex_attr);
+  mid_MsgQueue = osMessageQueueNew(QUEUE_SIZE, sizeof(uint8_t), NULL);
+
+  osDelay(osWaitForever);
+  while(1);
+}
+
+int main (void) {
+  // System Initialization
+  LEDInit(LED4 | LED3 | LED2 | LED1);
+  ButtonInit(USW1);
+  ButtonIntEnable(USW1);
+  ButtonInit(USW2);
+  ButtonIntEnable(USW2);
+
+  osKernelInitialize();                 // Initialize CMSIS-RTOS
+  osThreadNew(app_main, NULL, NULL);    // Create application main thread
+  
+  if (osKernelGetState() == osKernelReady) {
+    osKernelStart();                    // Start thread execution
+  }
+
+  while(1);
+}
